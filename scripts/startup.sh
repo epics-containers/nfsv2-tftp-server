c929ad73b6c5ac37d6abc6ea1cd426e6fc72b417
James Souter <james.souter@diamond.ac.uk>
2022-06-07 08:45:28 +0100

    fix updateexports bug, add EXPORT_PER_CLIENT env var

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -6,9 +6,12 @@ sed -i '/imklog/s/^/#/' /etc/rsyslog.conf
 #removes the imklog module, which provides kernel logging
 service rsyslog start
 
-[ -d /exports/nfs_server ] && echo "/exports/nfs_server 0.0.0.0/0.0.0.0(rw,sync,no_root_squash,insecure)" >> /etc/exports
-# [ -d /iocs ] && echo "/iocs 0.0.0.0/0.0.0.0(ro,sync,no_root_squash,insecure)" >> /etc/exports
-# [ -d /autosave ] && echo "/autosave 0.0.0.0/0.0.0.0(rw,sync,no_root_squash,insecure)" >> /etc/exports && \
+if [ "${EXPORT_PER_CLIENT}" != "true" ]; then
+[ -d /iocs ] && echo "/iocs 0.0.0.0/0.0.0.0(ro,sync,no_root_squash,insecure)" >> /etc/exports
+[ -d /autosave ] && echo "/autosave 0.0.0.0/0.0.0.0(rw,sync,no_root_squash,insecure)" >> /etc/exports
+fi
+
+
 chmod -R 777 /autosave 
 
 [ ! -f /etc/services ] && \
@@ -18,11 +21,15 @@ chmod -R 777 /autosave
 
 cp /scripts/nfs-user-server-init /etc/init.d/nfs-user-server
 
+if [ "${EXPORT_PER_CLIENT}" = "true" ]; then
 /scripts/updateexports.sh -d
+fi
 
 service rpcbind start
 service nfs-user-server start
 
 dnsmasq -p 0 --enable-tftp --tftp-single-port --tftp-root /iocs --log-facility=/var/log/syslog
 
+if [ "${EXPORT_PER_CLIENT}" = "true" ]; then
 /scripts/checkexports.sh
+fi
--- scripts/updateexports.sh
+++ scripts/updateexports.sh
@@ -12,7 +12,7 @@ done
 
 changesMade=0
 
-if [ ! -f /iocs/exports ]; then echo "/iocs/exports does not exist"; exit; fi
+if [ ! -f /iocs/exports ]; then echo "NFS: /iocs/exports does not exist" >> /var/log/syslog; exit; fi
 if [ ! -f /etc/exports ]; then touch /etc/exports; fi
 rm -f /etc/requiredexports
 cp /iocs/exports /etc/requiredexports
@@ -20,14 +20,13 @@ cp /iocs/exports /etc/requiredexports
 #writing to stdin stdout stderr (0, 1, 2)
 while IFS= read -r -u 9 line; do
 if [ -n "$line" ]; then
-    words=( "$line" )
-    iocName="${words[0]}"
-    iocIP="${words[1]}"
-    autosave="${words[2]}"
+    iocName="$(echo "$line" | awk '{print $1}')"
+    iocIP="$(echo "$line" | awk '{print $2}')"
+    autosave="$(echo "$line" | awk '{print $3}')"
 
     #check if /iocs/$iocName exists, and check if it's not already in /etc/exports
     #delete entries using same IP or IOC name if they exist
-    if [ -d /iocs/"$iocName" ] && ! grep "/iocs/$iocName $iocIP(" > /dev/null; then
+    if [ -d /iocs/"$iocName" ] && ! grep "/iocs/$iocName $iocIP(" /etc/exports > /dev/null; then
         sed -i "/iocs\/.*$iocIP(/d" /etc/exports; sed -i "/^\/iocs\/$iocName /d" /etc/exports;
         echo "/iocs/$iocName $iocIP(ro,sync,no_root_squash,insecure)" >> /etc/exports
         echo "Adding export: /iocs/$iocName $iocIP(ro,sync,no_root_squash,insecure)"
@@ -35,7 +34,7 @@ if [ -n "$line" ]; then
     fi
 
     #as above, but for autosave directories
-    if [ "$autosave" = "autosave" ] && [ -d /autosave/"$iocName" ] && ! grep "/autosave/$iocName $iocIP(" > /dev/null; then
+    if [ "$autosave" = "autosave" ] && [ -d /autosave/"$iocName" ] && ! grep "/autosave/$iocName $iocIP(" /etc/exports > /dev/null; then
         sed -i "/autosave\/.*$iocIP(/d" /etc/exports; sed -i "/^\/autosave\/$iocName /d" /etc/exports;
         echo "/autosave/$iocName $iocIP(rw,sync,no_root_squash,insecure)" >> /etc/exports
         echo "Adding export: /autosave/$iocName $iocIP(rw,sync,no_root_squash,insecure)"
