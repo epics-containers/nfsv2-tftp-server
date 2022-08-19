8b924d71a8ec19939224932176ee28d84f7e362a
Andy Wilson <andrew.wilson@diamond.ac.uk>
2022-08-19 14:27:24 +0100

    Only push on tag

--- kanikobuild.sh
+++ kanikobuild.sh
@@ -10,13 +10,9 @@ TEMPLATE_CMD="/kaniko/executor --context ${CI_PROJECT_DIR}/${SUBDIR}"
 CI_REGISTRY="gcr.io/diamond-privreg"
 DESTINATION="${CI_REGISTRY}/${GROUP}/${IMAGE_NAME}"
 
-if [[ -n "${CI_UPSTREAM_TAG}" ]]; then
-    # If there is an upstream tag, push a tagged image with that name.
-    CMD="${TEMPLATE_CMD} --dockerfile ${CI_PROJECT_DIR}/${SUBDIR}/Dockerfile --destination ${DESTINATION}:${CI_UPSTREAM_TAG}"
-elif [[ "${CI_UPSTREAM_BRANCH}" == master ]] || [[ -n "${CI_COMMIT_TAG}"]]; then
-    # If the upstream build is on the master branch, push a image with the 'latest' tag.
-    # Also allow updating 'latest' by pushing a tag on this repo.
-    CMD="${TEMPLATE_CMD} --dockerfile ${CI_PROJECT_DIR}/${SUBDIR}/Dockerfile --destination ${DESTINATION}:latest"
+if [[ -n "${CI_COMMIT_TAG}" ]]; then
+    # If there is a tag, push a tagged image with that name.
+    CMD="${TEMPLATE_CMD} --dockerfile ${CI_PROJECT_DIR}/${SUBDIR}/Dockerfile --destination ${DESTINATION}:${CI_COMMIT_TAG} --destination ${DESTINATION}:latest"
 else
     CMD="${TEMPLATE_CMD} --dockerfile ${CI_PROJECT_DIR}/${SUBDIR}/Dockerfile --no-push"
 fi
